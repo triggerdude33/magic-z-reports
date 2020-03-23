7637c21acc338e643174720ccb105c42845f42bd
Jakob Arvidsson <jakarv@kth.se>
2020-03-23 19:04:19 +0100

    Port to python3, add lists to handle products missing labels, update regex, add documentation (README and comments)

--- README.md
+++ README.md
@@ -1,14 +1,70 @@
 # magic-z-reports
-Tool for summation of z-reports created by iZettle cashregister.
+Tool for accounting of z-reports created by iZettle cashregister.
 
 ### Prerequisites
 You have to have the command `pdftotext` from the package `poppler-utils` installed to be able to run the script.
-During the creation of the script i used version `0.41.0` and it has thusly only been tested with said version.
+The script is tested on version `0.62.0`.
+
+### Product labels in iZettle
+The script currently supports 6 product labels. The labels are: `Öl, Cider, Sprit, Vin, Alkfritt, Mat`. The labels have to follow the format `[label],[product-name]`. A valid product name would for example look like this:
+`Cider, Somersby Pear`, `Sprit, 4 cl` or `Öl, Brooklyn, Brooklyn IPA`.  
+The script relies on these labels to produce the correct accounting assistance output.
+
+#### What to do if a product doesn't have a label?
+Products without a valid label are going to appear like this:
+```
+Bokföringshjälp:
+--------------------------
+  100.00 kr - Pop Art
+--------------------------
+OBS! OVANSTÅENDE RADER SAKNAR KATEGORI OCH MÅSTE DÄRFÖR HANTERAS MANUELLT!
+```
+If the product belongs to one of the labels it can be added manually to the corresponding list in the beginning of the script, in this case `list_Alkfritt`. Products added to these lists are treated as having the associated label.
+
+**Make sure to use the correct category when adding products manually!**
+
+These lists are only intended to work as a temporary fix, the labels should be updated in iZettle as soon as possible.
 
 ## Usage
 The script is used with one command.
-
-    python zreport.py "file1.pdf" ["file2.pdf" ...]
-
+```
+python3 zreport.py "file1.pdf" ["file2.pdf" ...]
+```
 You may specify more than one file if they are from the same date (event).
 The file paths may be either the full or the relative filepath.
+
+### Output
+Here is some example output:
+```
+-------------------------------------------
+Date: Feb 20, 2020
+Card: 16965.0   Cash: 0
+Refunds: 0.0   Total: 16965.0
+
+Bokföringshjälp:
+--------------------------
+   20.00 kr - Märke
+--------------------------
+OBS! OVANSTÅENDE RADER SAKNAR KATEGORI OCH MÅSTE DÄRFÖR HANTERAS MANUELLT!
+
+Försäljning (kredit)
+  240.00 kr - Försäljning läsk
+ 6600.00 kr - Försäljning öl
+ 2515.00 kr - Försäljning cider
+  220.00 kr - Försäljning vin
+ 5810.00 kr - Försäljning sprit
+ 1560.00 kr - Försäljning mat
+
+Inköp och Lager (Inköp på debet & Lager på kredit)
+ 5610.00 kr - Inköp öl & Öllager
+ 2137.75 kr - Inköp cider & Ciderlager
+ 2498.30 kr - Inköp sprit & Spritlager
+  187.00 kr - Inköp vin & Vinlager
+```
+
+### Troubleshooting
+The script will throw a date error if given z-reports with different dates. This is to prevent z-reports from different events accidentally being parsed together.
+
+The script throws a parsing error if the sum of all the sales for all the parsed products does not match the sum provided by iZettle. The most likely cause of this error is a broken regex. This could be due to an invalid product name or iZettle making changes to the z-report format.
+
+All the relevant debug-prints are left as comments in the script. When debugging a regex it helps to use an online regex tester like https://regexr.com/ or https://regex101.com/.
--- zreport.py
+++ zreport.py
@@ -2,73 +2,193 @@
 from subprocess import Popen, PIPE
 import sys, re, locale
 
+# Lista över produkter/kategorier som ska räknas som öl
+list_Ol = [
+"Öl",
+"pÖl",
+]
+
+# Lista över produkter/kategorier som ska räknas som cider
+list_Cider = [
+"PCider 2019",
+"Cider",
+"Cidraie original",
+"pCider",
+"Vikbo Cider Rabarber",
+"Crush",
+"Somersby Pear",
+"Sommersby Secco",
+"Vikbo Cider Rabarber",
+"Xide Kiwi Cucumber",
+]
+
+# Lista över produkter/kategorier som ska räknas som sprit
+list_Sprit = [
+"Sprit",
+]
+
+# Lista över produkter/kategorier som ska räknas som vin
+list_Vin = [
+"Vin",
+"Bubbel",
+"Aniara",
+"Freixenet Cava (glas)",
+"Freixenet Cava (hel flaska)",
+"Jacob’s Creek Chardonnay",
+"Prosecco FLASKA",
+"Les Oliviers Rosé (ett glas)",
+"Les Oliviers Rosé (hel flaska)",
+"Sun Gate Chardonnay (glas)",
+"Sun Gate Chardonnay (hel flaska)",
+"Jacob’s Creek Chardonnay Bubbel",
+]
+
+# Lista över produkter/kategorier som ska räknas som alkoholfritt
+list_Alkfritt = [
+"Alkfritt",
+"Läsk",
+"Pop Art",
+]
+
+# Lista över produkter/kategorier som ska räknas som mat
+list_Mat = [
+"Mat",
+]
 
 def main():
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')
     inp = ""
     for i in range(1,len(sys.argv)):
-        p = Popen(['pdftotext', '-layout', sys.argv[i], '-'], stdout=PIPE)
+        p = Popen(['pdftotext', '-layout', '-nopgbrk', sys.argv[i], '-'], stdout=PIPE)
         out, _ = p.communicate(None)
+        out = out.decode(encoding = 'UTF-8')
         if not 'date' in locals():
             date = getDate(out.split('\n'))
         if date and date == getDate(out.split('\n')):
             inp += out
         else:
             if date:
-                print "The reports don't have the same date! Error on file",'"{0}"'.format(sys.argv[i])
+                print("DATE ERROR: The reports don't have the same date! Error on file",'"{0}"'.format(sys.argv[i]))
                 return
+    
+    # Debug-print
+    #print(inp)
 
     lines = inp.split('\n')
-
-
     card, cash = getPayments(lines)
     discounts = getDiscounts(lines)
-    categorys = getProducts(lines)
+    categorys = getProductSales(lines)
+
+    # Debug-prints
+    #print(card)
+    #print(cash)
+    #print(discounts)
+    #print(categorys)
 
     if sum(categorys.values())+ discounts == (card+cash):
-        print "\n-------------------------------------------"
-	print "Date:", getDate(lines)
-        print "Card:", card, "  Cash:",cash
-        print "Refunds:", getNettoTotal(lines)-(card+cash), "  Total:", getNettoTotal(lines)
-        print ""
-        for k,v in categorys.items():
-            print '{0:8.2f} kr - {1}'.format(v, k)
+        print("\n-------------------------------------------")
+        print("Date:", getDate(lines))
+        print("Card:", card, "  Cash:",cash)
+        print("Refunds:", getNettoTotal(lines)-(card+cash), "  Total:", getNettoTotal(lines))
+        print("")
         if getNettoTotal(lines)-(card+cash) != 0:
-            print "ALERT!!! Report has refunds!!! Check report for more details"
+            print("ALERT!!! Report has refunds!!! Check report for more details")
         if discounts != 0:
-            print "ALERT!!! Report has disounts, make sure you handle them correctly"
-    	print "--------------------------\n"
-    	print "Bokföringshjälp:\n"
-        ol_lager = 0.0
-        cider_lager = 0.0
-    	for k,v in categorys.items():
-            if k == "Öl" or k == "pÖl":
-                ol_lager += v
-            if k == "Cider" or k == "pCider":
-                cider_lager += v
-            if k == "Sprit":
-                print '{0:8.2f} kr - {1}'.format(v*0.43, k + "lager")
+            print("ALERT!!! Report has disounts, make sure you handle them correctly")
+        print("Bokföringshjälp:")
+        print("--------------------------")
 
-        print '{0:8.2f} kr - {1}'.format(ol_lager*0.85, "Öllager")
-        print '{0:8.2f} kr - {1}'.format(cider_lager*0.85, "Ciderlager")
+        unhandledProducts = False
+        for k,v in categorys.items():
+            if k not in list_Ol and k not in list_Cider and k != "Sprit" and k!= "Mat" and k not in list_Vin and k not in list_Alkfritt:
+                print('{0:8.2f} kr - {1}'.format(v, k))
+                unhandledProducts = True
+        if unhandledProducts:
+            print("--------------------------")
+            print("OBS! OVANSTÅENDE RADER SAKNAR KATEGORI OCH MÅSTE DÄRFÖR HANTERAS MANUELLT!\n")
+
+        sold_Ol = 0.0
+        sold_Cider = 0.0
+        sold_Sprit = 0.0
+        sold_Vin = 0.0
+        sold_Alkfritt = 0.0
+        sold_Mat = 0.0
+        for k,v in categorys.items():
+            if k in list_Ol:
+                sold_Ol += v
+            if k in list_Cider:
+                sold_Cider += v
+            if k in list_Sprit:
+                sold_Sprit += v
+            if k in list_Vin:
+                sold_Vin += v
+            if k in list_Alkfritt:
+                sold_Alkfritt += v
+            if k in list_Mat:
+                sold_Mat += v
+
+        print("Försäljning (kredit)")
+        print('{0:8.2f} kr - {1}'.format(sold_Alkfritt, "Försäljning läsk"))
+        print('{0:8.2f} kr - {1}'.format(sold_Ol, "Försäljning öl"))
+        print('{0:8.2f} kr - {1}'.format(sold_Cider, "Försäljning cider"))
+        print('{0:8.2f} kr - {1}'.format(sold_Vin, "Försäljning vin"))
+        print('{0:8.2f} kr - {1}'.format(sold_Sprit, "Försäljning sprit"))
+        print('{0:8.2f} kr - {1}'.format(sold_Mat, "Försäljning mat"))
+        print("\nInköp och Lager (Inköp på debet & Lager på kredit)")
+        print('{0:8.2f} kr - {1}'.format(sold_Ol*0.85, "Inköp öl & Öllager"))
+        print('{0:8.2f} kr - {1}'.format(sold_Cider*0.85, "Inköp cider & Ciderlager"))
+        print('{0:8.2f} kr - {1}'.format(sold_Sprit*0.43, "Inköp sprit & Spritlager"))
+        print('{0:8.2f} kr - {1}'.format(sold_Vin*0.85, "Inköp vin & Vinlager"))
     else:
-        print "There seems to be some problem with the parsing of the file"
+        print("PARSING ERROR: There seems to be some problem with the parsing of the file")
+
+def getProductSales(lines):
+    """This function takes a list of lines and returns a map with the sales for each category.
+    The categories are not predefined but are instead created dynamically.
+    If a product does not have a category-prefix ("Öl," "Cider," etc) a new category is created using the product's name."""
+
+    # This regex is using 3 match groups. Group 1 retrieves the category, group 2 retrieves the sum without VAT, group 3 retrieves the sum with VAT.
+    # \s* before group 1 is to remove any leading whitespace.
+    # Group 1 matches everything up until a ',' or 3 consecutive spaces.
+    # \d+\s+ before group 2 filters out the number of items sold.
+    # \s+[\d,]+\d\d\s+ before group 3 filters out the VAT.
+    productregex = re.compile("\s*([A-ö+\s\d]*),?.+?\s{3}\d+\s+([\d\s]+,\d\d)\s+[\d,]+\d\d\s+([\d\s]+,\d\d)")
+    categorys = {}
+
+    sumSold  = 0
+    for l in lines:
+        match = productregex.match(l)
+        if match and match.group(2) == match.group(3):
+            sumSold += locale.atof(match.group(2).replace(' ',''))
+            category = match.group(1).strip()
+            if category in categorys.keys():
+                categorys[category] += locale.atof(match.group(2).replace(' ',''))
+                # Debug print
+                #print(match.group(1), match.group(2),match.group(3))
+            else:
+                categorys[category] = locale.atof(match.group(2).replace(' ',''))
+    
+    # Debug print
+    #print(categorys)
+    return categorys
 
 def getPayments(lines):
-    card_paymentsregex = re.compile("\s*Card\s+\(\d+\)\s+([\d,]+\.\d\d)\s*")
-    cash_paymentsregex = re.compile("\s*Cash\s+\(\d+\)\s+([\d,]+\.\d\d)\s*")
+    """This function takes a list of lines and returns a tuple with the total card and cash payments."""
+    card_paymentsregex = re.compile("\s*Card\s*\(\d+\)\s*([\d\s]+,\d\d)\s*")
+    cash_paymentsregex = re.compile("\s*Cash\s*\(\d+\)\s*([\d\s]+,\d\d)\s*")
     cash = 0
     card = 0
     for l in lines:
         card_match = card_paymentsregex.match(l)
         cash_match = cash_paymentsregex.match(l)
         if card_match:
-            card += locale.atof(card_match.group(1))
+            card += locale.atof(card_match.group(1).replace(' ',''))
         elif cash_match:
-            cash += locale.atof(cash_match.group(1))
+            cash += locale.atof(cash_match.group(1).replace(' ',''))
     return (card, cash)
 
 def getDiscounts(lines):
+    """This function takes a list of lines and returns the total sum of discounts"""
     dateregex = re.compile("\s*Discount\s+(-?\d+\.\d\d)\s+-?\d+\.\d\d\s+-?\d+\.\d\d\s*")
     for l in lines:
         match = dateregex.match(l)
@@ -77,6 +197,7 @@ def getDiscounts(lines):
     return 0;
 
 def getDate(lines):
+    """This function takes a list of lines and returns the date"""
     dateregex = re.compile("\s*.+?\s+([A-Z][a-z]{2} \d\d?, \d{4}) \d\d:\d\d\s*")
     for l in lines:
         match = dateregex.match(l)
@@ -84,30 +205,13 @@ def getDate(lines):
             return match.group(1)
 
 def getNettoTotal(lines):
-    nettoregex = re.compile("\s*Net amount\s+([\d,]+\.\d\d)\s+[\d,]+\.\d\d\s+[\d,]+\.\d\d\s*")
-    tot = 0
+    """This function takes a list of lines and returns the net sum of sales"""
+    nettoregex = re.compile("\s*Net amount\s+([\d\s]+,\d\d)\s+[\d,]+\d\d\s+[\d\s]+,\d\d\s*")
+    total = 0
     for l in lines:
         match = nettoregex.match(l)
         if match:
-            tot += locale.atof(match.group(1))  
-    return tot
-
-def getProducts(lines):
-    productregex = re.compile("(.+?)(,\s(.*?))?\s+([\d,]+)\s+([\d,]+\.\d\d)\s+[\d,]+\.\d\d\s+([\d,]+\.\d\d)")
-    categorys = {}
-
-    sumSold  = 0
-    for l in lines:
-        match = productregex.match(l)
-        if match and match.group(5) == match.group(6):
-            sumSold += locale.atof(match.group(5))
-            category = match.group(1).strip()
-            if category in categorys.keys():
-                categorys[category] += locale.atof(match.group(5))
-                #print match.group(1), match.group(3),match.group(4),match.group(5),(locale.atof(match.group(5))/locale.atoi(match.group(4)))
-            else:
-                categorys[category] = locale.atof(match.group(5))
-
-    return categorys
+            total += locale.atof(match.group(1).replace(' ',''))  
+    return total
 
 main()
